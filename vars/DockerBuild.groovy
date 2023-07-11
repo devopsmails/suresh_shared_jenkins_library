28480371521daee20642a917d62eac9082ce3a63
Devopsmails <devopsmails1@gmail.com>
2023-07-11 17:20:19 +0530

    Docker Build

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,8 +1,8 @@
 def call ( String ImageTag, String D_Hub_name, String ProjectName) {
 
     sh """
-    sudo -S docker image build -t ${(D_Hub_name)}/${(ProjectName)} .
-    sudo -S docker image tag ${(D_Hub_name)}/${(ProjectName)}:${(ImageTag)}
-    sudo -S docker image tag ${(D_Hub_name)}/${(ProjectName)}:latest
+    sudo docker image build -t ${(D_Hub_name)}/${(ProjectName)} .
+    sudo docker image tag ${(D_Hub_name)}/${(ProjectName)}:${(ImageTag)}
+    sudo docker image tag ${(D_Hub_name)}/${(ProjectName)}:latest
     """
 }
