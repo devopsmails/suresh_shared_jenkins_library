55b88b952030445b6bb3f827fb9029c5319007ac
Devopsmails <devopsmails1@gmail.com>
2023-07-12 22:06:27 +0530

    Docker Build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call( String project, String ImageTag, String hubUser) {
 
     sh """
-     docker image build -t ${(hubUser)}/${(project)} ./Dockerfile_suresh
+     docker image build -t ${(hubUser)}/${(project)} https://github.com/devopsmails/mrdevops_java_app.git/Dockerfile_suresh
      docker image tag ${(hubUser)}/${(project)} ${(hubUser)}/${(project)}:${ImageTag}
      docker image tag ${(hubUser)}/${(project)} ${(hubUser)}/${(project)}:latest
     """
