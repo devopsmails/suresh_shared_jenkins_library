8ad90853d0b4c6827022a0b0361e859caff9ca5f
Devopsmails <devopsmails1@gmail.com>
2023-07-13 15:13:46 +0530

    Docker image build ,push to ECR & Clean image

---
+++ vars/dockerPushECR.groovy
@@ -0,0 +1,7 @@
+def call( String aws_account_id, String region, String ecr_repo_name) {
+
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     docker tag ${ecr_repo_name}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
+    """
+} 
