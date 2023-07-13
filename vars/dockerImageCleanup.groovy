79171a208c1e1da8cc8767bbc71b971083d0658f
Devopsmails <devopsmails1@gmail.com>
2023-07-13 14:20:02 +0530

    Docker image clean up

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,4 +1,4 @@
-def call (String project, String ImageTag, String hubUse) {
+def call (String project, String ImageTag, String hubUser) {
 
     sh """
         docker rmi ${(hubUser)}/${(project)}:${ImageTag}
